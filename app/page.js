fcc84a6797841932ad7509504e26a43fbb278693
nesrin <nessrine@tekand.me>
2025-04-26 17:56:15 +0100

    congiguration of project:change font and add some color root

--- app/page.js
+++ app/page.js
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center mt-8">
       <h1>Hello</h1>
+      <div className=" w-50 h-50 border-b8  shadow-defaultCard rounded-lg">
+        <p className="text-defaultText">moi</p>
+      </div>
     </div>
   );
 }
