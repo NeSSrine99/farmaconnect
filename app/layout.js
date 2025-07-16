c60774a93ec22be49dfe30146874e53a262032ba
nesrin <103462967+Mnasrinesrine@users.noreply.github.com>
2025-07-16 15:27:11 +0100

    add icon of website

--- app/layout.js
+++ app/layout.js
@@ -10,24 +10,45 @@ import { CartProvider } from "./context/CartContext";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  variable: "--font-poppins", // هنا تعرف متغير جديد
+  variable: "--font-poppins",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata = {
   title: "Farmaconnect",
   description: "site web static de pharmacie en ligne ",
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+        </head>
         <body className={`${poppins.variable} antialiased`}>
           <CartProvider>
             <Header />
             <Navbar />
-            <main className="mx-4 ">
+            <main className="mx-4">
               {children}
               <Toaster position="top-right" />
             </main>
